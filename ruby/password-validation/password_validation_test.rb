236321934cfa03f52c364abd09c9823fc813a2c2
AldairMC <aldairmc2023@gmail.com>
2021-03-01 17:04:05 -0500

    :construction: Tests in progress

--- ruby/password-validation/password_validation_test.rb
+++ ruby/password-validation/password_validation_test.rb
@@ -4,7 +4,7 @@ require_relative 'password_validation'
 class TestPasswordValidation < MiniTest::Test
   def test_simple_string
     # skip
-    assert_equal 'weak', PasswordValidation.new('Name').validate
+    assert_equal 'weak', PasswordValidation.new('name').validate
   end
 
   def test_simple_number
@@ -16,4 +16,26 @@ class TestPasswordValidation < MiniTest::Test
     # skip
     assert_equal 'weak', PasswordValidation.new('abc123').validate
   end
+
+  def test_only_special_characters
+    # skip
+    assert_equal 'weak', PasswordValidation.new('#$%&/').validate
+  end
+
+  def test_combination_leters_and_special_characters
+    # skip
+    assert_equal 'weak', PasswordValidation.new('abcd#$%').validate
+  end
+
+  def test_combination_special_characters_and_number
+    # skip
+    assert_equal 'weak', PasswordValidation.new('$%&/1234').validate
+  end
+
+  def test_combination_leters_numbers_and_special_characters_but_length_six
+    # skip
+    assert_equal 'weak', PasswordValidation.new('ab#$12').validate
+  end
+
+
 end
